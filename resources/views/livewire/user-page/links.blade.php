1bb3067552425e6d331b8930105578fbdbce683a
erika <efockereinaldo@digitalocean.com>
2020-10-15 19:36:21 +0200

    slight improvement to user links page

--- resources/views/livewire/user-page/links.blade.php
+++ resources/views/livewire/user-page/links.blade.php
@@ -1,9 +1,8 @@
-<div class="flex flex-col bg-gray-200">
     @foreach ($links as $link)
-    <div class="text-gray-700 text-center bg-gray-400 mb-5">
+    <div class="text-gray-700 text-center bg-gray-100 p-3 mt-6 rounded-md">
         <div class="flex flex-row">
             <div class="m-5 px-4 py-2">
-                <img class="img-thumbnail w-20" src="/{{ $link->thumbnail }}" title="Link thumb">
+                <img class="img-thumbnail w-20 h-20 rounded-full" src="/{{ $link->thumbnail }}" title="Link thumb">
             </div>
 
             <div class="m-5 py-2">
@@ -15,6 +14,6 @@
 
     </div>
     @endforeach
-</div>
+
 
 
--- resources/views/livewire/user-page/show.blade.php
+++ resources/views/livewire/user-page/show.blade.php
@@ -1,14 +1,23 @@
 @extends('layouts.main')
 
 @section('content')
-<h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ $user->name }} {{$user->id}}
-    </h2>
+    <div class="w-full items-center">
+        <div class="text-gray-700 text-center p-3 mt-6 rounded-md">
+            <div class="flex flex-row">
+                <div class="m-5 px-4 py-2">
+                    <img class="img-thumbnail w-20 h-20 rounded-full" src="http://localhost:8000/images/nKa2EnjhYPITJBBbvj1qVLRqZwBr5DJKTmUpGqvh.png" title="Link thumb">
+                </div>
 
-    <div>
+                <div class="m-5 py-2">
+                    <h1>{{ $user->name }}</h1>
+                    <p>Check out {{ $user->username }}'s awesome links!</p>
+                </div>
+            </div>
+
+        </div>
+
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @livewire('user-page.links', ['user' => $user])
         </div>
-
     </div>
 @endsection
